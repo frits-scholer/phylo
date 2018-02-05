04e5d0093adc8286172c38cb2946594728dd9687
frits-scholer <frits.scholer@gmail.com>
2018-02-05 16:37:44 +0100

    solver with first set of constraints

--- cluster1.cpp
+++ cluster1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <algorithm>
 #include <string>
@@ -35,13 +36,38 @@ int main() {
     glp_set_col_kind(mip, i, GLP_BV);
     glp_set_obj_coef(mip, i, l);
   }
-  /*
-  vector<int> x(N);
-  vector<int> ia(N+1), ja(N+1);
-  vector<double> ar[N+1];
-  double Z;
-  */
+  int ia[1+N*N], ja[1+N*N];
+  double ar[1+N*N];
+  glp_add_rows(mip, N);
+  long indx{1};
+  for (int i=1;i<=N;i++) {
+    glp_set_row_bnds(mip, i, GLP_DB, 0.0, 1.0);
+    int cur_node;
+    is >> cur_node;
+    ia[indx]=i;ja[indx]=cur_node;ar[indx]=1;indx++;
+    cout << cur_node << ": ";
+    char ch;
+    is.get(ch);//:
+    string s;
+    getline(is,s);
+    istringstream iss(s);
+    while (true) {
+      iss >> cur_node;
+      if (!iss) break;
+      ia[indx]=i;ja[indx]=cur_node;ar[indx]=1;indx++;
+      cout << cur_node << " ";
+    }
+    cout << endl;
+  }
   is.close();
-
+  glp_load_matrix(mip, indx-1, ia, ja, ar);
+  glp_iocp parm;
+  glp_init_iocp(&parm);
+  parm.presolve = GLP_ON;
+  int err = glp_intopt(mip, &parm);
+  cout << "Object value: " << glp_mip_obj_val(mip) << endl;
+  for (int i=1;i<=N;i++) {
+    cout << glp_get_col_name(mip, i) << " = " << glp_mip_col_val(mip, i) << endl;
+  }
   glp_delete_prob(mip);
 }
